d06ff129c2eba3f7d390b2c751b79e599e3d953e
Rachel Fritz <rachelfritz0@gmail.com>
2020-07-07 11:12:33 -0500

    updated the other html and js files

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -12,12 +29,12 @@ var getRepoIssues = function (repo) {
 
         // check if api has paginated issues
         if (response.headers.get("Link")) {
-            console.log(displayWarning(repo));
+            displayWarning(repo);
         }
-            });
-        }
-        else {
-            alert("There was a problem with your request!");
+      });
+    }   else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -72,4 +89,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
